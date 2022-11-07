0a0db2ea62df565b63d2841d971282ea7c1882d4
VigilantBag <vigilantbag2018@gmail.com>
2022-11-07 06:52:19 -0800

    Make ocr text selectable

--- app/src/main/java/edu/csuci/lazynotetaker/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
+++ app/src/main/java/edu/csuci/lazynotetaker/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -304,9 +305,12 @@ fun AddEditNoteScreen(
 
 @Composable
 fun BodyContent() {
-    Text(
-        text = OCR.text,
-        fontSize = 22.sp
-    )
+    SelectionContainer() {
+        Text(
+            text = OCR.text,
+            fontSize = 22.sp
+        )
+    }
+
 }
 
